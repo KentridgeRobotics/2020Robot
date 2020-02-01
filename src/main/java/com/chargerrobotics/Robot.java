93e4a935c10d59f1e5c35e5deddfa4366e2229b4
Nicholas L <nicholas.h.leung@gmail.com>
2020-02-01 14:01:06 -0800

    Set default ManualDriveCommand in robot.java

--- src/main/java/com/chargerrobotics/Robot.java
+++ src/main/java/com/chargerrobotics/Robot.java
@@ -94,6 +94,7 @@ public class Robot extends TimedRobot {
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
+		robotContainer.setDefaultDriveCommand();
 	}
 
 	/**
