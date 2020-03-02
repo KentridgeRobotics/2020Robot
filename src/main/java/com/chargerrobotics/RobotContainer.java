eeca83297d4ef0bb3c0996c75f6ab58d92f4d69f
Josh O <kaio11602@gmail.com>
2020-03-01 17:18:05 -0800

    Bigger LED strip - Dimmer blue lights

--- src/main/java/com/chargerrobotics/RobotContainer.java
+++ src/main/java/com/chargerrobotics/RobotContainer.java
@@ -61,11 +61,11 @@ import com.chargerrobotics.utils.XboxController;
  */
 public class RobotContainer {
 
-	private static final boolean limelightEnabled = true;
-	private static final boolean driveEnabled = true;
+	private static final boolean limelightEnabled = false;
+	private static final boolean driveEnabled = false;
 	private static final boolean chomperEnabled = false;
-	private static final boolean shooterEnabled = true;
-	private static final boolean shooterHoodEnabled = true;
+	private static final boolean shooterEnabled = false;
+	private static final boolean shooterHoodEnabled = false;
 	private static final boolean colorSpinnerEnabled = false;
 	private static final boolean climberEnabled = false;
 
