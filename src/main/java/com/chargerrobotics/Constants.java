5dca685f234562229f3d88192436f6e947723e94
tpunn <56093054+tpunn@users.noreply.github.com>
2020-03-03 19:33:57 -0800

    created shoot all balls

--- src/main/java/com/chargerrobotics/Constants.java
+++ src/main/java/com/chargerrobotics/Constants.java
@@ -68,6 +68,8 @@ public final class Constants {
 	public static final double shooterVelocityGain = 0.0;
 	public static final double shooterMinOutput = -1.0;
 	public static final double shooterMaxOutput = 1.0;
+	
+	public static final double desiredDistance = 120.0;
 
 	// Shooter Hood Constants
 	public static final int hoodPIDLoopId = 0;
--- src/main/java/com/chargerrobotics/commands/groups/ShootAllBalls.java
+++ src/main/java/com/chargerrobotics/commands/groups/ShootAllBalls.java
@@ -9,9 +9,12 @@ package com.chargerrobotics.commands.groups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import com.chargerrobotics.commands.groups.ReadyShooter;
-import com.chargerrobotics.commands.autonomous.AutoMoveToDistance;
+import com.chargerrobotics.commands.groups.VisionDrive;
 import com.chargerrobotics.commands.shooter.HoodPresetAngleCommand;
 import com.chargerrobotics.commands.shooter.KickAllBalls;
+import com.chargerrobotics.subsystems.LimelightSubsystem;
+import com.chargerrobotics.subsystems.DriveSubsystem;
+import com.chargerrobotics.Constants;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -20,19 +23,19 @@ public class ShootAllBalls extends SequentialCommandGroup {
   /**
    * Creates a new ShootAllBalls.
    */
-  public ShootAllBalls() {
+  public ShootAllBalls(ReadyShooter shooter, VisionDrive drive, HoodPresetAngleCommand hood, KickAllBalls kick) {
     addCommands(
       // Ready the shooter (turn on shooter, turn towards target, turn on feeder, calibrate hood) - mock:
-      // new ReadyShooter(..., readyShooter),
+      shooter,
 
       // Move to preset distance robot is set for - mock:
-      // new AutoMoveToDistance(..., moveToDistance),
+      drive,
 
       // Set hood angle to preset - mock:
-      // new HoodPresetAngleCommand(..., hoodAngle),
+      hood,
 
       // Kick all the balls to shooter - mock:
-      // new KickAllBalls(..., kickBalls)
+      kick
     );
   }
 }
